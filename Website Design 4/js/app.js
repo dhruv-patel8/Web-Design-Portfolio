698200f2a8d0d7e6c524f53a9aca6872e023dfbb
Dhruv <dhruvdpatel@outlook.in>
2021-03-08 16:24:32 +0530

    My First Commit

---
+++ Website Design 4/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){       
+    $('.food-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 700,
+        autoplay:true,
+        autoplaySpeed: 2000,
+        arrows:false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+     });
+        
+            });
